2e09992b90a959bad22dca86a54cc73361b3e6da
Bohdan Sokulskyi <steelarcher@gmail.com>
2026-05-31 15:33:45 +0300

    No csp for dev.

--- src/Common/EventSubscriber/SecurityHeadersSubscriber.php
+++ src/Common/EventSubscriber/SecurityHeadersSubscriber.php
@@ -35,17 +35,16 @@ final readonly class SecurityHeadersSubscriber implements EventSubscriberInterfa
             return;
         }
 
-        $nonce = $this->nonceGenerator->getNonce();
-
         $headers = $event->getResponse()->headers;
         $headers->set('X-Content-Type-Options', 'nosniff');
         $headers->set('X-Frame-Options', 'DENY');
         $headers->set('X-XSS-Protection', '1; mode=block');
         $headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
         $headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
-        $headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'nonce-$nonce'; style-src 'self'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-ancestors 'none'; form-action 'self'; base-uri 'self'");
 
         if ($this->environment === 'prod') {
+            $nonce = $this->nonceGenerator->getNonce();
+            $headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'nonce-$nonce'; style-src 'self'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-ancestors 'none'; form-action 'self'; base-uri 'self'");
             $headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         }
     }
